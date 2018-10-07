193f3a6671773292e385e061eb5d7c73fc135430
Courtney Teed <courtneyteed@Courtneys-MBP.attlocal.net>
2018-10-06 21:44:19 -0700

    Center all text in main

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var main = document.createElement('main');
+main.classList.add('text-center');
+
 var pageBody = document.getElementsByTagName('body')[0];
 var scriptTag = document.getElementsByTagName('script')[0];
 pageBody.insertBefore(main, scriptTag);
